0cf9a2d70ad938a5b54c843ea7f2cf47451cefb0
Yasuaki Goto <yasu1003@gmail.com>
2016-09-17 17:08:40 +0900

    [fix] convert for swift2.3

--- Pod/Classes/JSSAlertView.swift
+++ Pod/Classes/JSSAlertView.swift
@@ -483,13 +483,13 @@ public extension UIImage {
 		UIGraphicsBeginImageContext(rect.size)
 		let context = UIGraphicsGetCurrentContext()
 		
-		CGContextSetFillColorWithColor(context, color.CGColor)
-		CGContextFillRect(context, rect)
+		CGContextSetFillColorWithColor(context!, color.CGColor)
+		CGContextFillRect(context!, rect)
 		
 		let image = UIGraphicsGetImageFromCurrentImageContext()
 		UIGraphicsEndImageContext()
 		
-		return image
+		return image!
 	}
 }
 
